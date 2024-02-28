2a7cfa0846bb43ffdd27422c1c83fed8b168abb9
come-senusi-wale <akinyemisaheedwale@gmailcom>
2024-02-28 11:38:32 +0100

    ok

--- src/data/faqData.js
+++ src/data/faqData.js
@@ -7,12 +7,12 @@ export const faq = {
      },
      {
          id:'2',
-         title:'What are the advantages of using BuildAfrica?',
+         title:'What are the advantages of joining BuildAfrica?',
          description:"BuildAfrica conducts a thorough identification and mapping exercise to locate active build communities operating outside of the Near ecosystem in Africa, particularly focusing on Nigeria. This ensures that no potential community is left undiscovered."
      },
      {
          id:'3',
-         title:'Do I need to know Near blockchain to use BuildAfrica?',
+         title:'Do I need to know Near blockchain to join BuildAfrica?',
          description:"No, you do not necessarily need to have prior knowledge of the Near blockchain to use BuildAfrica. BuildAfrica aims to provide a comprehensive onboarding process for communities interested in participating in the Near ecosystem, starting with Nigeria. Through its targeted educational programs, collaborations with partners, and integration initiatives, BuildAfrica equips participants with the necessary skills and knowledge to engage effectively with Near's technology and opportunities."
      },
      {
